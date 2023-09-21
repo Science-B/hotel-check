9c3987ed3d8d105c334a101828f180c91b6470a0
Science-B <dmitriytwd@gmail.com>
2023-09-21 12:32:10 +0300

    added get-hotels-data

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { setupStore } from './redux/store.ts'
 import App from './App.tsx'
 
 import './global.scss'
 
+const store = setupStore()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>,
+	</Provider>,
 )
---
+++ src/services/hotels-service.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+import { Hotel } from '../api/interfaces';
+
+const hotelService = {
+	get: async (city: string, checkIn: string, checkOut: string) => {
+		console.log('FROM SERVICE', city, checkIn, checkOut);
+		const { data } = await axios.get<Hotel[]>(
+			`http://engine.hotellook.com/api/v2/cache.json?location=${city}&currency=rub&checkIn=${checkIn}&checkOut=${checkOut}&limit=10`,
+		);
+		return data;
+	},
+};
+
+export default hotelService;
