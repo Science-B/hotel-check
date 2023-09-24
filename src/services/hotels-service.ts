cd0d2ba8de5a6eb7b5d5d4818e159fa6560e9587
Science-B <dmitriytwd@gmail.com>
2023-09-24 12:48:59 +0300

    added fix-hotelservice

--- src/services/hotels-service.ts
+++ src/services/hotels-service.ts
@@ -3,8 +3,11 @@ import axios from 'axios';
 import { Hotel } from '../api/interfaces';
 
 const hotelService = {
-	get: async (city: string, checkIn: string, checkOut: string) => {
-		console.log('FROM SERVICE', city, checkIn, checkOut);
+	get: async (
+		city: string,
+		checkIn: string,
+		checkOut: string,
+	): Promise<Hotel[]> => {
 		const { data } = await axios.get<Hotel[]>(
 			`http://engine.hotellook.com/api/v2/cache.json?location=${city}&currency=rub&checkIn=${checkIn}&checkOut=${checkOut}&limit=10`,
 		);
